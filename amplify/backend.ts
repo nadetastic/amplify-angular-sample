73d35a123affbbafa68b003ed95b566327209189
Dan Kiuna <dkkiuna@amazon.com>
2024-11-20 17:39:31 -0600

    update cdk and amplify

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,7 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { CfnApi } from 'aws-cdk-lib/aws-appsync';
 
 defineBackend({
   auth,
