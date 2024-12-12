46fbfa57098b2ce11ef75442acc6869d2b33af0b
Dan Kiuna <dkkiuna@amazon.com>
2024-12-12 09:22:15 -0600

    adding frontend only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,29 @@ import outputs from '../../amplify_outputs.json';
 
 Amplify.configure(outputs);
 
+const latestConfig = Amplify.getConfig();
+
+Amplify.configure({
+  ...latestConfig,
+  API: {
+    ...latestConfig.API,
+    Events: {
+      endpoint: '',
+      apiKey: '',
+      defaultAuthMode: 'apiKey',
+    },
+  },
+});
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
   imports: [RouterOutlet, TodosComponent],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
 }
+
+// npx ampx generate graphql-client-code --app-id d86ff4rs16sgr	--branch main
