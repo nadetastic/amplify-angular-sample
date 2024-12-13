ef908fc4171d18edca63093c3851a54c8fc85843
Dan Kiuna <dkkiuna@amazon.com>
2024-12-13 08:21:38 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
+import { generateClient } from 'aws-amplify/data';
 import outputs from '../../amplify_outputs.json';
+import * as queries from '../graphql/queries';
 
 Amplify.configure(outputs);
 
@@ -29,6 +31,12 @@ Amplify.configure({
 })
 export class AppComponent {
   title = 'amplify-angular-template';
-}
+  client: ReturnType<typeof generateClient> = generateClient();
 
-// npx ampx generate graphql-client-code --app-id d86ff4rs16sgr	--branch main
+  async getAll() {
+    await this.client.graphql({
+      query: queries.listTodos,
+      authMode: 'userPool',
+    });
+  }
+}
